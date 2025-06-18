7758ea2915852a749ee4eec1bd0b62938c82bf08
10_days_till_xmas <x10_days_till_xmas@outlook.com>
2025-06-18 03:00:22 +0100

    Add helper class for making temporary directories

---
+++ UltraModding.Templates.Tests/TempDir.cs
@@ -0,0 +1,34 @@
+﻿using Xunit.Abstractions;
+
+namespace UltraModding.Templates.Tests;
+/// <summary>
+/// Temporary directory class for creating and managing a temporary directory.
+/// </summary>
+public sealed class TempDir : IDisposable
+{
+    private readonly string path;
+    public ITestOutputHelper? Output { get; init; }
+    public TempDir()
+    {
+        path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        Directory.CreateDirectory(path);
+    }
+    public TempDir(string dirName)
+    {
+        path = Path.Combine(Path.GetTempPath(), dirName);
+        Directory.CreateDirectory(path);
+    }
+    public static implicit operator string(TempDir tempDir) => tempDir.path;
+    public override string ToString() => path;
+
+    public void Dispose()
+    {
+        if (!Directory.Exists(path))
+        {
+            Output?.WriteLine($"Temporary directory '{path}' does not exist, nothing to delete.");
+            return;
+        }
+        Directory.Delete(path, true);
+        Output?.WriteLine($"Temporary directory '{path}' deleted successfully.");
+    }
+}
